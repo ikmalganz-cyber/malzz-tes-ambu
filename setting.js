e07211cb2764c3a34e3e67f88cae17900ee5cbe3
ikmalganz-cyber <stokmalzganteng@gmail.com>
2026-04-09 14:05:12 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,2 +1,2 @@
-global.apikey = process.env.VPEDIA_API_KEY || '';
-global.feenya = parseInt(process.env.FEE || '0');
+global.apikey = process.env.VPEDIA_API_KEY || 'KhafaTopUp_yjmm32s92qz3jn0d';
+global.feenya = parseInt(process.env.FEE || '350');
